9c0391c0ee147b37e937b71f04d3462124e268ed
hs286 <hamzakhalid4992@gmail.com>
2024-07-08 08:42:20 +0500

    table sort | virtualization | searching | hide / show

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="card">
+    <DataTable :value="cars" scrollable scrollHeight="400px" :virtualScrollerOptions="{ itemSize: 46 }"
+      tableStyle="min-width: 50rem" v-model:filters="filters" filterDisplay="menu" :loading="loading"
+      :globalFilterFields="['vin']" removableSort sortMode="multiple">
+      <template #header>
+        <Settings :columns="columns" @toggle-column-visibility="toggleColumnVisibility" />
+
+        <div class="flex justify-start gap-3 my-2">
+          <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilter" />
+          <IconField>
+            <InputIcon>
+              <i class="pi pi-search" />
+            </InputIcon>
+            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+          </IconField>
+        </div>
+      </template>
+      <Column v-for="(col, index) of columns" :field="col.field" :style="col.visible === false ? 'display: none;' : ''"
+        :header="col.header" sortable :key="col.field + '_' + index">
+      </Column>
+
+    </DataTable>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { CarService } from '@/service/CarService';
+import { FilterMatchMode, FilterOperator } from '@primevue/core/api';
+import Settings from '../src/components/ColumnSettings.vue';
+
+const cars = ref([]);
+const columns = ref([
+  { field: 'id', header: 'Id', visible: true },
+  { field: 'vin', header: 'Vin', visible: true },
+  { field: 'year', header: 'Year', visible: true },
+  { field: 'brand', header: 'Brand', visible: true },
+  { field: 'color', header: 'Color', visible: true },
+]);
+
+
+const loading = ref(false);
+const filters = ref({});
+
+const initFilters = () => {
+  filters.value = {
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    vin: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+    },
+  };
+};
+
+initFilters();
+
+const clearFilter = () => {
+  initFilters();
+};
+
+onMounted(async () => {
+  loading.value = true;
+  cars.value = await fetchCars();
+  loading.value = false;
+});
+
+async function fetchCars() {
+  return Array.from({ length: 10000 }).map((_, i) => CarService.generateCar(i + 1));
+}
+
+const onToggle = (val) => {
+  columns.value = val;
+};
+
+const toggleColumnVisibility = (key) => {
+  columns.value = columns.value.map((col) => {
+    if (col.field === key) {
+      col.visible = !col.visible
+      return col
+    }
+    return col
+  });
+  console.log(columns)
+};
+
+</script>
+
+<style scoped>
+.draggable-column {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.draggable-column li {
+  padding: 0.5rem;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  cursor: grab;
+}
+</style>
